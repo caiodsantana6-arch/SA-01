4d40d10c92f0105701f7cc4bc924dad76b822bf7
caiodsantana6-arch <caio.d.santana6@ba.estudante.senai.br>
2026-06-25 20:55:23 -0300

    Update RecursoNaoEncontradoException.java

--- controlechamados/controlechamados/src/main/java/br/com/senai/controlechamados/exception/RecursoNaoEncontradoException.java
+++ controlechamados/controlechamados/src/main/java/br/com/senai/controlechamados/exception/RecursoNaoEncontradoException.java
@@ -1,4 +1,9 @@
 package br.com.senai.controlechamados.exception;
 
-public class RecursoNaoEncontradoException {
+public class RecursoNaoEncontradoException extends RuntimeException {
+
+    public RecursoNaoEncontradoException(String mensagem) {
+        super(mensagem);
+    }
+
 }
