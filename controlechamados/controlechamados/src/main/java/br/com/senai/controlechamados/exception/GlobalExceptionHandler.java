bd1d5d5b4e59dc46523b2e44efb3dac53f614705
caiodsantana6-arch <caio.d.santana6@ba.estudante.senai.br>
2026-06-25 20:56:11 -0300

    Update GlobalExceptionHandler.java

--- controlechamados/controlechamados/src/main/java/br/com/senai/controlechamados/exception/GlobalExceptionHandler.java
+++ controlechamados/controlechamados/src/main/java/br/com/senai/controlechamados/exception/GlobalExceptionHandler.java
@@ -1,4 +1,44 @@
 package br.com.senai.controlechamados.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(RecursoNaoEncontradoException.class)
+    public ResponseEntity<ErrorResponse> tratarRecursoNaoEncontrado(RecursoNaoEncontradoException ex) {
+
+        ErrorResponse erro = new ErrorResponse(
+                HttpStatus.NOT_FOUND.value(),
+                "Recurso não encontrado",
+                ex.getMessage());
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(erro);
+    }
+
+    @ExceptionHandler(RegraNegocioException.class)
+    public ResponseEntity<ErrorResponse> tratarRegraNegocio(RegraNegocioException ex) {
+
+        ErrorResponse erro = new ErrorResponse(
+                HttpStatus.BAD_REQUEST.value(),
+                "Regra de negócio",
+                ex.getMessage());
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(erro);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> tratarErroGenerico(Exception ex) {
+
+        ErrorResponse erro = new ErrorResponse(
+                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                "Erro interno",
+                ex.getMessage());
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(erro);
+    }
+
 }
