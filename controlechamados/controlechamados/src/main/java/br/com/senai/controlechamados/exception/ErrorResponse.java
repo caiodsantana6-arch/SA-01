06fb66620f9ed3705b75cd4923db0bda8e779fe7
caiodsantana6-arch <caio.d.santana6@ba.estudante.senai.br>
2026-06-25 20:54:50 -0300

    Update ErrorResponse.java

--- controlechamados/controlechamados/src/main/java/br/com/senai/controlechamados/exception/ErrorResponse.java
+++ controlechamados/controlechamados/src/main/java/br/com/senai/controlechamados/exception/ErrorResponse.java
@@ -1,4 +1,41 @@
 package br.com.senai.controlechamados.exception;
 
 public class ErrorResponse {
+
+    private int status;
+    private String erro;
+    private String mensagem;
+
+    public ErrorResponse() {
+    }
+
+    public ErrorResponse(int status, String erro, String mensagem) {
+        this.status = status;
+        this.erro = erro;
+        this.mensagem = mensagem;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getErro() {
+        return erro;
+    }
+
+    public void setErro(String erro) {
+        this.erro = erro;
+    }
+
+    public String getMensagem() {
+        return mensagem;
+    }
+
+    public void setMensagem(String mensagem) {
+        this.mensagem = mensagem;
+    }
 }
