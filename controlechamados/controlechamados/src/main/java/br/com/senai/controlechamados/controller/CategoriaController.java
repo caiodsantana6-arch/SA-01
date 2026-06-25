af847b822a46fe71ca06c969acd89294a9001daf
caiodsantana6-arch <caio.d.santana6@ba.estudante.senai.br>
2026-06-25 20:06:16 -0300

    Update CategoriaController.java

--- controlechamados/controlechamados/src/main/java/br/com/senai/controlechamados/controller/CategoriaController.java
+++ controlechamados/controlechamados/src/main/java/br/com/senai/controlechamados/controller/CategoriaController.java
@@ -1,4 +1,48 @@
 package br.com.senai.controlechamados.controller;
 
+import br.com.senai.controlechamados.dto.CategoriaRequestDTO;
+import br.com.senai.controlechamados.dto.CategoriaResponseDTO;
+import br.com.senai.controlechamados.service.CategoriaService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/categorias")
 public class CategoriaController {
+
+    private final CategoriaService service;
+
+    public CategoriaController(CategoriaService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public List<CategoriaResponseDTO> listar() {
+        return service.listar();
+    }
+
+    @GetMapping("/{id}")
+    public CategoriaResponseDTO buscarPorId(@PathVariable Long id) {
+        return service.buscarPorId(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public CategoriaResponseDTO salvar(@RequestBody CategoriaRequestDTO dto) {
+        return service.salvar(dto);
+    }
+
+    @PutMapping("/{id}")
+    public CategoriaResponseDTO atualizar(@PathVariable Long id,
+                                          @RequestBody CategoriaRequestDTO dto) {
+        return service.atualizar(id, dto);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void excluir(@PathVariable Long id) {
+        service.excluir(id);
+    }
 }
