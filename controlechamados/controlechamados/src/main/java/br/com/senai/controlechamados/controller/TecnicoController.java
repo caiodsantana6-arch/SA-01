b2740a12ea0f2610c9d8b28807f82c2641845d8b
caiodsantana6-arch <caio.d.santana6@ba.estudante.senai.br>
2026-06-25 20:07:20 -0300

    Update TecnicoController.java

--- controlechamados/controlechamados/src/main/java/br/com/senai/controlechamados/controller/TecnicoController.java
+++ controlechamados/controlechamados/src/main/java/br/com/senai/controlechamados/controller/TecnicoController.java
@@ -1,4 +1,48 @@
 package br.com.senai.controlechamados.controller;
 
+import br.com.senai.controlechamados.dto.TecnicoRequestDTO;
+import br.com.senai.controlechamados.dto.TecnicoResponseDTO;
+import br.com.senai.controlechamados.service.TecnicoService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/tecnicos")
 public class TecnicoController {
+
+    private final TecnicoService service;
+
+    public TecnicoController(TecnicoService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public List<TecnicoResponseDTO> listar() {
+        return service.listar();
+    }
+
+    @GetMapping("/{id}")
+    public TecnicoResponseDTO buscarPorId(@PathVariable Long id) {
+        return service.buscarPorId(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public TecnicoResponseDTO salvar(@RequestBody TecnicoRequestDTO dto) {
+        return service.salvar(dto);
+    }
+
+    @PutMapping("/{id}")
+    public TecnicoResponseDTO atualizar(@PathVariable Long id,
+                                        @RequestBody TecnicoRequestDTO dto) {
+        return service.atualizar(id, dto);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void excluir(@PathVariable Long id) {
+        service.excluir(id);
+    }
 }
