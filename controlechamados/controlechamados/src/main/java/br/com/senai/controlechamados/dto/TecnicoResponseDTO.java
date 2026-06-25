657c59284cdcb1396d6be82198ba5b37670e4dff
Mateus Sena <mateussantos192007@gmail.com>
2026-06-25 17:56:40 -0300

    Rename TecnioResponseDTO -> TecnicoResponseDTO

--- controlechamados/controlechamados/src/main/java/br/com/senai/controlechamados/dto/TecnicoResponseDTO.java
+++ controlechamados/controlechamados/src/main/java/br/com/senai/controlechamados/dto/TecnicoResponseDTO.java
@@ -2,28 +2,30 @@ package br.com.senai.controlechamados.dto;
 
 import br.com.senai.controlechamados.enums.Ativo;
 
-public class TecnioResponseDTO {
+public class TecnicoResponseDTO {
     private Long id;
+    private String nome;
     private String email;
     private String especialidade;
     private Ativo ativo;
 
-    public TecnioResponseDTO() {
+    public TecnicoResponseDTO() {
     }
 
-    public TecnioResponseDTO(Long id, String email, String especialidade, Ativo ativo) {
-        this.id = id;
+    public TecnicoResponseDTO(Ativo ativo, String email, String especialidade, Long id, String nome) {
+        this.ativo = ativo;
         this.email = email;
         this.especialidade = especialidade;
-        this.ativo = ativo;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
         this.id = id;
+        this.nome = nome;
+    }
+
+    public Ativo getAtivo() {
+        return ativo;
+    }
+
+    public void setAtivo(Ativo ativo) {
+        this.ativo = ativo;
     }
 
     public String getEmail() {
@@ -42,11 +44,19 @@ public class TecnioResponseDTO {
         this.especialidade = especialidade;
     }
 
-    public Ativo getAtivo() {
-        return ativo;
+    public Long getId() {
+        return id;
     }
 
-    public void setAtivo(Ativo ativo) {
-        this.ativo = ativo;
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
     }
 }
