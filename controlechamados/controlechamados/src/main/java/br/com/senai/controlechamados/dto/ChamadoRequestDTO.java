4eb31dede5dacc3378199efc154cf7435c72c48d
Mateus Sena <mateussantos192007@gmail.com>
2026-06-26 03:19:24 -0300

    Update ChamadoRequestDTO.java

--- controlechamados/controlechamados/src/main/java/br/com/senai/controlechamados/dto/ChamadoRequestDTO.java
+++ controlechamados/controlechamados/src/main/java/br/com/senai/controlechamados/dto/ChamadoRequestDTO.java
@@ -2,42 +2,39 @@ package br.com.senai.controlechamados.dto;
 
 import br.com.senai.controlechamados.entity.Categoria;
 import br.com.senai.controlechamados.entity.Tecnico;
+import br.com.senai.controlechamados.enums.Prioridade;
 import br.com.senai.controlechamados.enums.StatusChamado;
+
+import java.util.List;
 
 public class ChamadoRequestDTO {
     private String titulo;
     private String descricao;
     private String solicitante;
     private String local;
-    private String prioridade;
-    private StatusChamado status;
-    private String dataAbertura;
-    private String dataFinalizacao;
-    private Categoria categoria;
-    private Tecnico tecnicos;
+    private Prioridade prioridade;
+    private Long categoriaId;
+    private List<Long> tecnicosIds;
 
     public ChamadoRequestDTO() {
     }
 
-    public ChamadoRequestDTO(String titulo, String descricao, String solicitante, String local, String prioridade, StatusChamado status, String dataAbertura, String dataFinalizacao, Categoria categoria, Tecnico tecnicos) {
+    public ChamadoRequestDTO(String titulo, String descricao, String solicitante, String local, Prioridade prioridade, Long categoriaId, List<Long> tecnicos) {
         this.titulo = titulo;
         this.descricao = descricao;
         this.solicitante = solicitante;
         this.local = local;
         this.prioridade = prioridade;
-        this.status = status;
-        this.dataAbertura = dataAbertura;
-        this.dataFinalizacao = dataFinalizacao;
-        this.categoria = categoria;
-        this.tecnicos = tecnicos;
+        this.categoriaId = categoriaId;
+        this.tecnicosIds = tecnicos;
     }
 
-    public String getTitulo() {
-        return titulo;
+    public Long getCategoriaId() {
+        return categoriaId;
     }
 
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
+    public void setCategoriaId(Long categoriaId) {
+        this.categoriaId = categoriaId;
     }
 
     public String getDescricao() {
@@ -48,14 +45,6 @@ public class ChamadoRequestDTO {
         this.descricao = descricao;
     }
 
-    public String getSolicitante() {
-        return solicitante;
-    }
-
-    public void setSolicitante(String solicitante) {
-        this.solicitante = solicitante;
-    }
-
     public String getLocal() {
         return local;
     }
@@ -64,51 +53,35 @@ public class ChamadoRequestDTO {
         this.local = local;
     }
 
-    public String getPrioridade() {
+    public Prioridade getPrioridade() {
         return prioridade;
     }
 
-    public void setPrioridade(String prioridade) {
+    public void setPrioridade(Prioridade prioridade) {
         this.prioridade = prioridade;
     }
 
-    public StatusChamado getStatus() {
-        return status;
+    public String getSolicitante() {
+        return solicitante;
     }
 
-    public void setStatus(StatusChamado status) {
-        this.status = status;
+    public void setSolicitante(String solicitante) {
+        this.solicitante = solicitante;
     }
 
-    public String getDataAbertura() {
-        return dataAbertura;
+    public List<Long> getTecnicosIds() {
+        return tecnicosIds;
     }
 
-    public void setDataAbertura(String dataAbertura) {
-        this.dataAbertura = dataAbertura;
+    public void setTecnicosIds(List<Long> tecnicosIds) {
+        this.tecnicosIds = tecnicosIds;
     }
 
-    public String getDataFinalizacao() {
-        return dataFinalizacao;
+    public String getTitulo() {
+        return titulo;
     }
 
-    public void setDataFinalizacao(String dataFinalizacao) {
-        this.dataFinalizacao = dataFinalizacao;
-    }
-
-    public Categoria getCategoria() {
-        return categoria;
-    }
-
-    public void setCategoria(Categoria categoria) {
-        this.categoria = categoria;
-    }
-
-    public Tecnico getTecnicos() {
-        return tecnicos;
-    }
-
-    public void setTecnicos(Tecnico tecnicos) {
-        this.tecnicos = tecnicos;
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
     }
 }
