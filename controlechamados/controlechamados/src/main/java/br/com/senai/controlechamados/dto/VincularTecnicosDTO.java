7ae150b08a42dfdfdc2e4e4514212352b2ad48d6
Mateus Sena <mateussantos192007@gmail.com>
2026-06-26 03:18:34 -0300

    Add VincularTecnicosDTO

--- controlechamados/controlechamados/src/main/java/br/com/senai/controlechamados/dto/VincularTecnicosDTO.java
+++ controlechamados/controlechamados/src/main/java/br/com/senai/controlechamados/dto/VincularTecnicosDTO.java
@@ -1,4 +1,17 @@
 package br.com.senai.controlechamados.dto;
 
+import java.util.List;
+
 public class VincularTecnicosDTO {
+    private List<Long> tenicosIds;
+
+    public VincularTecnicosDTO(){}
+
+    public List<Long> getTenicosIds() {
+        return tenicosIds;
+    }
+
+    public void setTenicosIds(List<Long> tenicosIds) {
+        this.tenicosIds = tenicosIds;
+    }
 }
