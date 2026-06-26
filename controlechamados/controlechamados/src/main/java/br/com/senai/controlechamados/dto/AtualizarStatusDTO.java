0d86fcb267ba972586d1f8b11c0f249d4d49658a
Mateus Sena <mateussantos192007@gmail.com>
2026-06-26 03:17:52 -0300

    Add AtualizarStatusDTO

--- controlechamados/controlechamados/src/main/java/br/com/senai/controlechamados/dto/AtualizarStatusDTO.java
+++ controlechamados/controlechamados/src/main/java/br/com/senai/controlechamados/dto/AtualizarStatusDTO.java
@@ -1,4 +1,17 @@
 package br.com.senai.controlechamados.dto;
 
+import br.com.senai.controlechamados.enums.StatusChamado;
+
 public class AtualizarStatusDTO {
+    private StatusChamado status;
+
+    public AtualizarStatusDTO() {}
+
+    public StatusChamado getStatus() {
+        return status;
+    }
+
+    public void setStatus(StatusChamado status) {
+        this.status = status;
+    }
 }
